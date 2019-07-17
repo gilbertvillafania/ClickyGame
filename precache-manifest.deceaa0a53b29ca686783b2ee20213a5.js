2b83391d9a88d2f7a18363e1e8d55b2c5001b9ec
botong719 <botong719@gmail.com>
2019-07-16 18:55:44 -0700

    Updates

---
+++ precache-manifest.deceaa0a53b29ca686783b2ee20213a5.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de263bd1eeb3a116a59a7395ae97c1de",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "52a497b7dca0dead2c85",
+    "url": "/ClickyGame/static/css/main.60c2dfc4.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/ClickyGame/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "52a497b7dca0dead2c85",
+    "url": "/ClickyGame/static/js/main.5062b9b1.chunk.js"
+  },
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  },
+  {
+    "revision": "c8cea731af3d045a43603b147691053d",
+    "url": "/ClickyGame/static/media/andy.c8cea731.JPG"
+  },
+  {
+    "revision": "1a0f7c5b4d7ae153a9fc1e5580e5c3ad",
+    "url": "/ClickyGame/static/media/ann.1a0f7c5b.JPG"
+  },
+  {
+    "revision": "f49918067b0fa673880fead1394e31fc",
+    "url": "/ClickyGame/static/media/april.f4991806.JPG"
+  },
+  {
+    "revision": "3bf7efa98eb88e24783516c57d7e29e5",
+    "url": "/ClickyGame/static/media/ben.3bf7efa9.JPG"
+  },
+  {
+    "revision": "84fa916a4a808999486a1ed48d56a242",
+    "url": "/ClickyGame/static/media/chris.84fa916a.JPG"
+  },
+  {
+    "revision": "e089b3aecda5263349a0d311c4f3699f",
+    "url": "/ClickyGame/static/media/craig.e089b3ae.JPG"
+  },
+  {
+    "revision": "8ac4a6a7e3b440e7e9480e1b47918873",
+    "url": "/ClickyGame/static/media/donna.8ac4a6a7.JPG"
+  },
+  {
+    "revision": "62bf9147596fe418092a6510d5ea2781",
+    "url": "/ClickyGame/static/media/jerry.62bf9147.JPG"
+  },
+  {
+    "revision": "8737fb22190be707d331ea1559d8246c",
+    "url": "/ClickyGame/static/media/leslie.8737fb22.JPG"
+  },
+  {
+    "revision": "3fe3d6a4f4b46615a780ea7aab718dae",
+    "url": "/ClickyGame/static/media/mona-lisa.3fe3d6a4.JPG"
+  },
+  {
+    "revision": "c7a32e4e0982893ef9e8f06078a5ad45",
+    "url": "/ClickyGame/static/media/ron.c7a32e4e.JPG"
+  },
+  {
+    "revision": "44ebc61b1249205bc448b1e57e60cc31",
+    "url": "/ClickyGame/static/media/tom.44ebc61b.JPG"
+  }
+]);
